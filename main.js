71f569fcadce453c63e0adc4d1feff6736ceec26
Sean Murakami <murakami.sean@yahoo.com>
2018-09-06 13:10:54 -0700

    #1 updated the js file to make the descriptions fall to the bottom && updated the css to include a different font and a navigation bar

--- main.js
+++ main.js
@@ -1,64 +1,112 @@
 let app = {
   view: 'catelog',
-  items: [
-    {
-      brand: 'Drum Brand',
-      name: 'Drum Name',
-      price: '$100',
-      image: 'https://www.shareicon.net/data/256x256/2015/11/25/183468_drum_256x256.png'
-    },
-    {
-      brand: 'Drum Brand',
-      name: 'Drum Name',
-      price: '$100',
-      image: 'https://www.shareicon.net/data/256x256/2015/11/25/183468_drum_256x256.png'
-    },
-    {
-      brand: 'Drum Brand',
-      name: 'Drum Name',
-      price: '$100',
-      image: 'https://www.shareicon.net/data/256x256/2015/11/25/183468_drum_256x256.png'
-    },
-    {
-      brand: 'Drum Brand',
-      name: 'Drum Name',
-      price: '$100',
-      image: 'https://www.shareicon.net/data/256x256/2015/11/25/183468_drum_256x256.png'
-    },
-    {
-      brand: 'Drum Brand',
-      name: 'Drum Name',
-      price: '$100',
-      image: 'https://www.shareicon.net/data/256x256/2015/11/25/183468_drum_256x256.png'
-    },
-    {
-      brand: 'Drum Brand',
-      name: 'Drum Name',
-      price: '$100',
-      image: 'https://www.shareicon.net/data/256x256/2015/11/25/183468_drum_256x256.png'
-    }
-  ]
+  catalog: {
+    items: [
+      {
+        itemId: 1,
+        brand: 'Ludwig',
+        name: 'Black Beauty',
+        price: 749.99,
+        description: 'A snare drum for people with too many drumsticks.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'United States',
+        imageUrl: 'https://goo.gl/ubY0QA'
+      },
+      {
+        itemId: 2,
+        name: 'Darbuka',
+        brand: 'Vatan',
+        price: 79.99,
+        description: 'A hand drum for people who like belly dancing.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'Turkey',
+        imageUrl: 'http://www.eminpercussion.com/site/urunfoto/darbuka_bakir_1.png'
+      },
+      {
+        itemId: 3,
+        name: 'Djembe',
+        brand: 'Remo',
+        price: 49.99,
+        description: 'A hand drum for people with dreadlocks.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'West Africa',
+        imageUrl: 'https://goo.gl/iKtU8x'
+      },
+      {
+        itemId: 4,
+        name: 'Hi-Hats',
+        brand: 'Paiste',
+        price: 400,
+        description: 'A set of hats for people who spare no expense.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'Switzerland',
+        imageUrl: 'https://goo.gl/LV9l8O'
+      },
+      {
+        itemId: 5,
+        name: 'Giant Step',
+        brand: 'Sonor',
+        price: 599,
+        description: 'A pedal for metal drummers with only one foot.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'Germany',
+        imageUrl: 'https://goo.gl/zsFo1q'
+      },
+      {
+        itemId: 6,
+        name: 'Travis Barker Signature',
+        brand: 'Zildjian',
+        price: 24.99,
+        description: 'A set of drumsticks from the 90s.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'United States',
+        imageUrl: 'https://goo.gl/Cro3tL'
+      },
+      {
+        itemId: 7,
+        name: 'Bodhran',
+        brand: 'Meinl',
+        price: 129.99,
+        description: 'A frame drum for people who like to jig.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'Ireland',
+        imageUrl: 'https://goo.gl/YNb74O'
+      },
+      {
+        itemId: 8,
+        name: 'TR-09',
+        brand: 'Roland',
+        price: 399,
+        description: 'A drum machine for people with no desk space.',
+        details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        origin: 'Japan',
+        imageUrl: 'https://goo.gl/gJYUxz'
+      }
+    ]
+  }
 }
 
 function renderItem(catalogItem, number) {
+  let searchCatalog = catalogItem.catalog.items
   let $item =
-    createElement('div', {class: 'card col-sm-2', style: 'width: 18rem;'}, [
-      createElement('img', {class: 'card-img-top', src: catalogItem.items[number].image, alt: 'Card image cap'}, []),
-      createElement('div', {class: 'card-body'}, [
-        createElement('h5', {class: 'card-title'}, [catalogItem.items[number].brand]),
-        createElement('p', {class: 'card-text'}, [catalogItem.items[number].name]),
-        createElement('p', {class: 'card-text'}, [catalogItem.items[number].price])
+    createElement('div', {class: 'card col-lg-2 border-success mt-4 pt-4'}, [
+      createElement('img', {class: 'card-img-top', style: 'max-height: 10rem', src: searchCatalog[number].imageUrl, alt: 'image cap'}, []),
+      createElement('div', {class: 'card-body text-center d-flex flex-column'}, [
+        createElement('h5', {class: 'mt-auto card-title'}, [searchCatalog[number].brand]),
+        createElement('p', {class: 'card-text'}, [searchCatalog[number].name]),
+        createElement('p', {class: 'card-text text-success font-weight-bold'}, [searchCatalog[number].price])
       ])
     ])
   return $item
 }
 
-function renderAllItems(catalog) {
-  let $container = createElement('div', {class: 'container'}, [])
-  let $header = createElement('h1', {}, ['Jamazon'])
+function renderAllItems(allItems) {
+  let $container = createElement('div', {class: 'container mt-3'}, [])
+  let $header = createElement('h1', {class: 'font-weight-light'}, ['Jamazon'])
   let $row = createElement('row', {class: 'row'}, [])
-  for (let i = 0; i < catalog.items.length; i++) {
-    $row.appendChild(renderItem(catalog, i))
+  let searchCatalog = allItems.catalog.items
+  for (let i = 0; i < searchCatalog.length; i++) {
+    $row.appendChild(renderItem(allItems, i))
   }
   $container.appendChild($header)
   $container.appendChild($row)
